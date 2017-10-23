b7eea6a5bf4313812ae15a249a69ab6618d887a3
Paul Hine <paul.hine@gmail.com>
2017-10-23 18:17:30 -0400

    log req hostname

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(compression(), basicAuth({
   realm: 'Return to Cinder'
 }), express.static('./public'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/*', (req, res) => {
+  console.log('req host', req.headers.hostname);
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.get('/oauth', (req, res) => {
   console.log(req);
