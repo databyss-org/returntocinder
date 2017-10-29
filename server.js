cd130771ef6edf247fa3c0596f8dfcd02db51730
Paul Hine <paul.hine@gmail.com>
2017-10-29 18:33:50 -0400

    dropbox-webhook

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const basicAuth = require('express-basic-auth');
 const compression = require('compression');
 const path = require('path');
+const bodyParser = require('body-parser');
+const Dropbox = require('dropbox');
 
 const app = express();
 
@@ -13,6 +15,7 @@ const middleware = [
 ];
 
 app.use(...middleware);
+app.use(bodyParser.json());
 
 app.get('/dropbox-webhook', (req, res) => {
   res.send(req.query.challenge);
@@ -30,3 +33,14 @@ app.get('/*', (req, res) => {
 app.listen(app.get('port'), () => {
   console.log('server started on port', app.get('port'));
 });
+
+function checkDocRevisions() {
+  const dropbox = new Dropbox({ accessToken: process.env.DBX });
+  dropbox.filesListRevisions({
+    path: '/Baby Daddy/BBDD.rtf'
+  }).then((res) => {
+    console.log(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
