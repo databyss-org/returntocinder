11b3f1a23e2d4f4a1ada2b79b56104ea807c443a
Paul Hine <paul.hine@gmail.com>
2017-10-29 23:23:33 -0400

    content

--- src/content/about/index.js
+++ src/content/about/index.js
@@ -1,4 +1,38 @@
 import frontis from './frontis.json';
 import menu from './menu.json';
+import grammar from './grammar.json';
+import epigraphs from './epigraphs.json';
+import manifest from './manifest.json';
+import contact from './contact.json';
 
-export { frontis, menu };
+const bibliography = (props) => {
+  const content = {
+    title: 'bibliography'
+  };
+  if (!props.appState.biblio) {
+    return {
+      ...content,
+      body: ['loading...']
+    };
+  }
+  const biblioList = Object.keys(props.appState.biblio).reduce((list, sid) =>
+    list.concat(props.appState.biblio[sid]), []
+  );
+  biblioList.sort((a, b) => a.id < b.id ? -1 : 1);
+  return {
+    title: 'bibliography',
+    body: biblioList.map(b => (
+      `${b.id} ${b.citations[0]} ${b.citations.join('<br />')}`
+    ))
+  };
+};
+
+export {
+  frontis,
+  menu,
+  grammar,
+  epigraphs,
+  manifest,
+  bibliography,
+  contact
+};
