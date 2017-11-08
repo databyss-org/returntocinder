ed7d64033570db4700ad89cc5104cca04e3197ea
Paul Hine <paul.hine@gmail.com>
2017-11-08 02:17:17 +0100

    update content output paths

--- src/content/index.js
+++ src/content/index.js
@@ -1,31 +1,31 @@
 export default [
   {
     path: '/Baby Daddy/BBDD.rtf',
-    out: './public/full.json'
+    out: './repo/public/full.json'
   },
   {
     path: '/Baby Daddy/content/about/contact.json',
-    out: './src/content/about/contact.json',
+    out: './repo/src/content/about/contact.json',
     compile: true
   },
   {
     path: '/Baby Daddy/content/about/epigraphs.json',
-    out: './src/content/about/epigraphs.json',
+    out: './repo/src/content/about/epigraphs.json',
     compile: true
   },
   {
     path: '/Baby Daddy/content/about/frontis.json',
-    out: './src/content/about/frontis.json',
+    out: './repo/src/content/about/frontis.json',
     compile: true
   },
   {
     path: '/Baby Daddy/content/about/grammar.json',
-    out: './src/content/about/grammar.json',
+    out: './repo/src/content/about/grammar.json',
     compile: true
   },
   {
     path: '/Baby Daddy/content/about/manifest.json',
-    out: './src/content/about/manifest.json',
+    out: './repo/src/content/about/manifest.json',
     compile: true
   }
 ];
