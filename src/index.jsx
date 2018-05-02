17072a225c7458804db777dfa4e50a49d9d673e6
Paul Hine <paul.hine@gmail.com>
2018-05-02 23:39:21 +0200

    Add buggyfill

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
 import 'reset-css/reset.css';
 import store from './redux/configureStore';
 
 import Main from './components/Main.jsx';
+
+viewportUnitsBuggyfill.init();
 
 render(
   <Provider store={store}>
