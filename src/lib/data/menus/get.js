7b8703ca72f4267256bd25797ec4a3d1d72fc0f2
Paul Hine <paul.hine@gmail.com>
2018-06-18 00:54:10 -0400

    Sort menus by ordinal

--- src/lib/data/menus/get.js
+++ src/lib/data/menus/get.js
@@ -1,7 +1,7 @@
 import { list } from '../mongo';
 
 export default async (path) => {
-  const menus = await list('menus', { path });
+  const menus = await list('menus', { path }, { ordinal: 1 });
   if (!menus.length) {
     throw new Error(`Menu not found with path: ${path}`);
   }
