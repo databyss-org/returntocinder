6d309d08a0fa1aea031dfcdfedfba316f7c5e27a
Paul Hine <paul@hine.works>
2019-02-17 17:41:23 +0100

    Bump Mongo version to 4.0.2

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -5,38 +5,31 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import UploadRtf from './UploadRtf';
 import DbActions from './DbActions';
+import MotifLinks from './MotifLinks';
 
 const App = () => (
   <Router>
     <div>
       <Navbar>
         <Navbar.Header>
-          <Navbar.Brand>
-            Databyss Admin
-          </Navbar.Brand>
+          <Navbar.Brand>Databyss Admin</Navbar.Brand>
         </Navbar.Header>
         <Nav>
           <NavItem>
-            <Link to='/admin/supplement'>
-              Upload Supplement
-            </Link>
+            <Link to="/admin/supplement">Upload Supplement</Link>
           </NavItem>
           <NavItem>
-            <Link to='/admin/db'>
-              Database Actions
-            </Link>
+            <Link to="/admin/db">Database Actions</Link>
           </NavItem>
         </Nav>
       </Navbar>
       <div style={{ padding: '20px' }}>
-        <Route path='/admin/supplement' component={UploadRtf} />
-        <Route path='/admin/db' component={DbActions} />
+        <Route path="/admin/supplement" component={UploadRtf} />
+        <Route path="/admin/db" component={DbActions} />
+        <Route path="/admin/motiflinks" component={MotifLinks} />
       </div>
     </div>
   </Router>
 );
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
