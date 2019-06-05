5df77a46b0c798d3cbbc2599161805726ebf116b
Michael <michaelsenejoa@gmail.com>
2019-06-04 20:50:19 -0500

    biblio page gets author scroll links

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,36 +1,58 @@
-import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
-import { compose } from 'redux'
-import { withRouter } from 'react-router-dom'
-import Subnav from './Subnav.jsx'
-import styles from '../app.scss'
-import actions from '../redux/app/actions'
-import withLoader from '../hoc/withLoader'
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { withRouter } from 'react-router-dom';
+import Subnav from './Subnav.jsx';
+import styles from '../app.scss';
+import actions from '../redux/app/actions';
+import withLoader from '../hoc/withLoader';
 
 class Page extends PureComponent {
   constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    document.addEventListener('click', this.handleClick)
+    document.addEventListener('click', this.handleClick);
   }
   componentWillUnmount() {
-    document.removeEventListener('click', this.handleClick)
+    document.removeEventListener('click', this.handleClick);
   }
 
   handleClick(e) {
     //This handler checks to see whether a biblio link was checked
-    const targetLink = e.target.closest('a')
-    targetLink &&
-      !targetLink.getAttribute('href').match(/^(http|https)/) &&
-      (e.preventDefault(),
-      this.props.history.push(targetLink.getAttribute('href')))
+    const targetLink = e.target.closest('a');
+    if (!targetLink.getAttribute('href')) {
+      e.preventDefault();
+    } else {
+      targetLink &&
+        !targetLink.getAttribute('href').match(/^(http|https)/) &&
+        (e.preventDefault(),
+        this.props.history.push(targetLink.getAttribute('href')));
+    }
+  }
+
+  authorNameClick(id) {
+    let el = document.getElementById(id);
+    el.scrollIntoView({
+      behavior: 'smooth',
+    });
   }
 
   render() {
-    let { subnavPath, content, menu, useHash } = this.props
+    let { subnavPath, content, menu, useHash, appState } = this.props;
+    let { authorList } = appState;
+    const authorListHeader = authorList.map((a, i) => (
+      <a
+        className={styles.authorContent}
+        id={i}
+        onClick={() => this.authorNameClick(a.id)}
+      >
+        {a.lastName}, {a.firstName}
+      </a>
+    ));
+
     return (
       <div className={styles.about}>
         <div className={styles.container}>
@@ -39,6 +61,16 @@ class Page extends PureComponent {
           </div>
           <header>{content.title}</header>
           <div className={styles.body} id='about'>
+            {content.title === 'bibliography' && (
+              <div className={styles.authorContainer}>
+                <div className={styles.authorTitle}>
+                  <h1>Authors</h1>
+                </div>
+                <br />
+                {authorListHeader}
+              </div>
+            )}
+
             {content.body.map((para, idx) => (
               <p key={idx} dangerouslySetInnerHTML={{ __html: para }} />
             ))}
@@ -64,7 +96,7 @@ class Page extends PureComponent {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -91,4 +123,4 @@ export default withRouter(
       showLoader: true,
     })
   )(Page)
-)
+);
