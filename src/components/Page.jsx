c7462cd0778ed5de9a488b90fa43435048162fe3
Michael <michaelsenejoa@gmail.com>
2019-06-15 13:36:03 -0400

    added withRouter updated version

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,10 +1,11 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import Subnav from './Subnav.jsx';
-import styles from '../app.scss';
-import actions from '../redux/app/actions';
-import withLoader from '../hoc/withLoader';
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { withRouter } from 'react-router-dom'
+import Subnav from './Subnav.jsx'
+import styles from '../app.scss'
+import actions from '../redux/app/actions'
+import withLoader from '../hoc/withLoader'
 
 class Page extends PureComponent {
   constructor(props) {
@@ -173,28 +174,30 @@ class Page extends PureComponent {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default compose(
-  connect(
-    state => ({
-      appState: state.app,
-    }),
-    actions
-  ),
-  withLoader({
-    propsToLoad: props => ({
-      content: props.contentFunc
-        ? props.contentFunc(props)
-        : props.appState.pages[props.path],
-      menu: props.appState.menus[props.subnavPath],
-    }),
-    loaderActions: props => ({
-      content: () => props.fetchPage(props.path),
-      menu: () => props.fetchMenu(props.subnavPath),
-    }),
-    showLoader: true,
-  })
-)(Page);
+export default withRouter(
+  compose(
+    connect(
+      state => ({
+        appState: state.app,
+      }),
+      actions
+    ),
+    withLoader({
+      propsToLoad: props => ({
+        content: props.contentFunc
+          ? props.contentFunc(props)
+          : props.appState.pages[props.path],
+        menu: props.appState.menus[props.subnavPath],
+      }),
+      loaderActions: props => ({
+        content: () => props.fetchPage(props.path),
+        menu: () => props.fetchMenu(props.subnavPath),
+      }),
+      showLoader: true,
+    })
+  )(Page)
+)
