6d408f5c8d07aa7921bfbf853bdf183159274169
Michael <michaelsenejoa@gmail.com>
2019-07-04 11:28:26 -0400

    authors list

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import { connect } from 'react-redux';
-import styles from '../app.scss';
-import Author from './Author';
+import React from 'react'
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
+import styles from '../app.scss'
+import Author from './Author'
 
 const Authors = ({ history, authorList }) => (
   <ul className={styles.frontAuthors}>
@@ -10,8 +10,8 @@ const Authors = ({ history, authorList }) => (
       <Author key={author.id} author={author} />
     ))}
   </ul>
-);
+)
 
 export default withRouter(
   connect(state => ({ authorList: state.app.authorList }))(Authors)
-);
+)
--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -1,39 +1,39 @@
 /* eslint-disable arrow-body-style */
-import React from 'react';
-import { Link, withRouter, matchPath, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import cx from 'classnames';
-import styles from '../app.scss';
-import Authors from './Authors';
-import Motifs from './Motifs';
-import actions from '../redux/app/actions';
+import React from 'react'
+import { Link, withRouter, matchPath, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import cx from 'classnames'
+import styles from '../app.scss'
+import Authors from './Authors'
+import Motifs from './Motifs'
+import actions from '../redux/app/actions'
 
 class Front extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      show: 'Motifs' // 'Motifs' || 'Authors'
-    };
-    this.showMotifs = this.showMotifs.bind(this);
-    this.showAuthors = this.showAuthors.bind(this);
+      show: 'Motifs', // 'Motifs' || 'Authors'
+    }
+    this.showMotifs = this.showMotifs.bind(this)
+    this.showAuthors = this.showAuthors.bind(this)
   }
 
   showMotifs() {
-    this.state.show !== 'Motifs' ? this.setState({ show: 'Motifs' }) : '';
+    this.state.show !== 'Motifs' ? this.setState({ show: 'Motifs' }) : ''
   }
 
   showAuthors() {
-    this.state.show !== 'Authors' ? this.setState({ show: 'Authors' }) : '';
+    this.state.show !== 'Authors' ? this.setState({ show: 'Authors' }) : ''
   }
 
   render() {
-    const { location, toggleSearchIsFocused, app } = this.props;
+    const { location, toggleSearchIsFocused, app } = this.props
     return (
       <div
         className={cx(styles.front, {
           [styles.showFull]: Boolean(
             matchPath(location.pathname, { path: '/', exact: true })
-          )
+          ),
         })}
       >
         <div className={cx(styles.container, styles.withMotifs)}>
@@ -42,19 +42,23 @@ class Front extends React.Component {
             <p>
               <span dangerouslySetInnerHTML={{ __html: app.pages['/'].body }} />
               &nbsp;
-              <Link to="/about/frontis">&hellip;</Link>
+              <Link to='/about/frontis'>&hellip;</Link>
             </p>
             <div className={styles.toggles}>
               <a onClick={this.showMotifs}>Motifs</a>
               <a onClick={this.showAuthors}>Authors</a>
             </div>
           </div>
-          <div className={cx(styles.body, styles.show)}>
+          <div
+            className={cx(styles.body, styles.show, {
+              [styles.authorsBody]: this.state.show !== 'Motifs',
+            })}
+          >
             {this.state.show === 'Motifs' ? <Motifs /> : <Authors />}
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 export default withRouter(
@@ -62,4 +66,4 @@ export default withRouter(
     state => state,
     actions
   )(Front)
-);
+)
