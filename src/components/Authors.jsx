4a2a213f3b744ddebfcea2b4ed22bea590cc0fc3
Michael <michaelsenejoa@gmail.com>
2019-07-04 12:54:18 -0400

    author list fix

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -4,13 +4,32 @@ import { connect } from 'react-redux'
 import styles from '../app.scss'
 import Author from './Author'
 
-const Authors = ({ history, authorList }) => (
-  <ul className={styles.frontAuthors}>
-    {authorList.map(author => (
-      <Author key={author.id} author={author} />
-    ))}
-  </ul>
-)
+const Authors = ({ history, authorList }) => {
+  let list = authorList.slice()
+  let length = Math.ceil(list.length / 2)
+  var splitList = []
+  while (list.length) {
+    splitList.push(list.splice(0, length))
+  }
+
+  return (
+    <div style={{ display: '-webkit-box' }}>
+      <ul className={styles.frontAuthors}>
+        {splitList[0] &&
+          splitList[0].map(author => (
+            <Author key={author.id} author={author} />
+          ))}
+      </ul>
+
+      <ul className={styles.frontAuthors}>
+        {splitList[1] &&
+          splitList[1].map(author => (
+            <Author key={author.id} author={author} />
+          ))}
+      </ul>
+    </div>
+  )
+}
 
 export default withRouter(
   connect(state => ({ authorList: state.app.authorList }))(Authors)
